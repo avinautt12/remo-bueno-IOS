fa6bd9a37c789df8e729d91492a6b8754b23c09f
avinautt12 <avinaluciano50@gmail.com>
2025-03-03 16:22:44 -0600

    Initial Commit

---
+++ repo-bueno-IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  repo-bueno-IOS
+//
+//  Created by mac on 03/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
