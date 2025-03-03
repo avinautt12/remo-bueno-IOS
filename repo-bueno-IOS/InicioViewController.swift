64c443bb431bc6fead255daba70d1b941f34f09d
avinautt12 <avinaluciano50@gmail.com>
2025-03-03 17:56:20 -0600

    avance

---
+++ repo-bueno-IOS/InicioViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InicioViewController.swift
+//  repo-bueno-IOS
+//
+//  Created by mac on 03/03/25.
+//
+
+import UIKit
+
+class InicioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
